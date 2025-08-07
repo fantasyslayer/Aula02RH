f6a0e8db3597c7eef38e1a6198b88f139a5dda65
unknown <discov126@gmail.com>
2025-08-07 14:18:58 -0300

    Aula02

---
+++ Models/Funcionario.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Aula02RH.Models.Enuns;
+
+namespace Aula02RH.Models
+{
+    public class Funcionario
+    {
+        public int Id { get; set;}
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public DateTime DataAdmissao { get; set; }
+        public decimal Salario { get; set; }
+        public TipoFuncionarioEnum TipoFuncionario { get; set; }
+    
+    
+    public void ReajustarSalario(){
+        Salario = Salario + (Salario * 10/100);
+    }
+
+    public string ExibirPeriodoExperiencia(){
+        string PeriodoExperiencia = string.Format("Periodo de experiencia: {0} até {1}",DataAdmissao,DataAdmissao.AddMonths(3));
+        return PeriodoExperiencia;
+    }
+    
+    public decimal CalcularDescontoVT(decimal percentual){
+        decimal desconto = this.Salario * percentual/100;
+        return desconto;
+    }
+    }
+}
